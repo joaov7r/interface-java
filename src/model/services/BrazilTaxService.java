2328be605db98f32864501c1433ccd47793419c4
João Vitor <jvc.lopes@outlook.com>
2024-08-15 09:35:38 -0300

    Implementing Interface

--- src/model/services/BrazilTaxService.java
+++ src/model/services/BrazilTaxService.java
@@ -1,8 +1,8 @@
 package model.services;
 
-public class BrazilTaxService {
+public class BrazilTaxService implements TaxService{
 
-	public Double tax(double amount) {
+	public double tax(double amount) {
 		if(amount <= 100.0) {
 			return amount * 0.2;
 		}
